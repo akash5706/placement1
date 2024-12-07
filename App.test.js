10420c8607e899b4eface3f0e227cee67d48e929
akash5706 <126559234+akash5706@users.noreply.github.com>
2024-12-07 08:56:33 +0530

    Update App.test.js

--- App.test.js
+++ App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  test('should render the "learn react" link', () => {
+    render(<App />);
+    const link = screen.getByRole('link', { name: /learn react/i });
+    expect(link).toBeInTheDocument();
+  });
 });
+
