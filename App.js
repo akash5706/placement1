aef53796705b89f470a2f208c9d706f0e541cbc6
akash5706 <126559234+akash5706@users.noreply.github.com>
2024-12-07 08:58:18 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,25 +5,17 @@ import RoleManagement from "./components/RoleManagement";
 import PermissionsManagement from "./components/PermissionsManagement";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const Layout = ({ children }) => {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="Content">{children}</div>
-    </div>
-  );
-};
-
 function App() {
   return (
     <Router>
-      <Layout>
+      <div className="App">
+        <Navbar />
         <Routes>
           <Route path="/users" element={<UserManagement />} />
           <Route path="/roles" element={<RoleManagement />} />
           <Route path="/permissions" element={<PermissionsManagement />} />
         </Routes>
-      </Layout>
+      </div>
     </Router>
   );
 }
